3d236e9a667d1197dc0c3bfa6a534f1968d50f39
charlesdrews <charlesdrews@gmail.com>
2016-06-20 11:24:20 -0400

    Add starter code and readme

---
+++ README.md
@@ -0,0 +1,35 @@
+---
+title: Data Types and Variables
+type: Homework
+duration: "2:00"
+creator:
+    name: Charlie Drews
+    city: NYC
+---
+
+
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Data Types and Variables
+
+## Homework
+
+#### Requirements
+
+- Fork this repository (use the "Fork" button in the upper right)
+- Clone this repository to your computer
+    * Click the green "Clone or Download" button and copy the https url for the repo
+    * Use the cd command in your terminal to navigate to where you want to download the repo on your machine
+    * Use the command "git clone <url>" where you replace <url> with the actual url you copied
+- Open the starter code and modify Main.java to complete the code per instructions in the comments for problems 1 and 2a-c
+- Save Main.java and tomorrow we'll learn how to submit via git and github
+
+**Bonus:**
+- Complete problem 3 per instructions in the comments in the starter code
+
+#### Starter code
+
+Open IntelliJ, click "Open" and select the `starter-code` folder in the repo you cloned to your machine
+
+#### Deliverable
+
+Tomorrow we'll learn how to _commit_ your code in git, then _push_ it to github, then finally submit a _pull request_ to submit your code to us. For now, just save your code and remember where it is :)
+
---
+++ starter-code/src/Main.java
@@ -0,0 +1,82 @@
+/**
+ * Starter code for Data Types and Variables Homework
+ * Please complete the code per the instructions below.
+ *
+ * Created by charlie on 6/20/16.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Reminder: comments are anything after // on a single line
+        // or anything on one or more lines between /* and */
+
+
+        /**
+         * 1) Fill in an appropriate data type for the following variable declarations based on the initialized value.
+         *
+         * Hint - the data type of the variable must match the type of the value being assigned to that variable!)
+         *
+         * Hint 2 - try running the code (right click on this file and select "Run Main.main()" and if you get
+         * zero errors then your data types and values all match up
+         */
+        ___ varA = 3;
+        ___ varB = 9.75;
+        ___ varC = 4.0;
+        ___ varD = 2.;
+        ___ varE = 8573f;
+        ___ varF = "Gelato is delicious!";
+        ___ varG = '$';
+        ___ varH = "$";
+        ___ varI = true;
+        ___ varJ = false;
+
+
+        /**
+         * 2a) Imagine you need a variable named "price" to represent the cost of an item in an online store.
+         * Declare the variable price below, then on the next line assign some value to price.
+         *
+         * Hint - in the code above we did the declaration and the initialization (or initial assignment
+         * of a value) both on the same line. But those are actually two distinct steps that could also
+         * be done in separate lines of code.
+         */
+
+        ___ price;
+        price = ___;
+
+
+        /**
+         * 2b) Next, image you need a variable for customer comments on an item. Declare comment below and
+         * give it an initial value of "placeholder comment" all in one line of code.
+         */
+
+        ___ comment = ___;
+
+
+        /**
+         * 2c) You also need a variable to represent the item ID. The id will be an int, and the value is
+         * shown below, but it's important that no other developers change this ID, so you need to
+         * add a keyword which tells the Java compiler this value cannot be changed once set.
+         */
+
+        ___ int itemID = 12345;
+
+
+        /**
+         * BONUS
+         *
+         * 3) Predict what the result of the division operation below. Compare your prediction to what
+         * is printed to the terminal. If they are the same, explain how you made your prediction in
+         * a comment. If they are different, modify the code below so that it will produce the result
+         * you expect.
+         *
+         * Hint - google "integer division"
+         */
+
+        int numeratorInt = 5;
+        int divisorInt = 2;
+        int divisionResultInt = numeratorInt / divisorInt;
+        System.out.println(divisionResultInt);
+
+    }
+}
